3e2853a0e91b76e75153245b5bfa668efd6dc252
Siddhartha Mitra <mitra.siddhartha@gmail.com>
2025-03-24 20:10:20 -0400

    latest

--- mod/x86_64/adaptive_AMPA.c
+++ mod/x86_64/adaptive_AMPA.c
@@ -382,7 +382,7 @@ extern void _cvode_abstol( Symbol**, double*, int);
  pnt_receive[_mechtype] = _net_receive;
  pnt_receive_size[_mechtype] = 5;
  	hoc_register_var(hoc_scdoub, hoc_vdoub, hoc_intfunc);
- 	ivoc_help("help ?1 adaptive_AMPA /www/projects/Spillover/mod/adaptive_AMPA.mod\n");
+ 	ivoc_help("help ?1 adaptive_AMPA /u/sid/Spillover_2/mod/adaptive_AMPA.mod\n");
  hoc_register_limits(_mechtype, _hoc_parm_limits);
  hoc_register_units(_mechtype, _hoc_parm_units);
  }
@@ -772,7 +772,7 @@ _first = 0;
 }
 
 #if NMODL_TEXT
-static const char* nmodl_filename = "/www/projects/Spillover/mod/adaptive_AMPA.mod";
+static const char* nmodl_filename = "/u/sid/Spillover_2/mod/adaptive_AMPA.mod";
 static const char* nmodl_file_text = 
   "TITLE simple AMPA receptors\n"
   "\n"
